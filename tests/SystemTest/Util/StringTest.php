c43d3b5a5149c6357cf0e2335de3c2c31601d096
Diego Saouda <diegosaouda@gmail.com>
2014-03-16 08:11:01 -0300

    Falhando coverge

--- tests/SystemTest/Util/StringTest.php
+++ tests/SystemTest/Util/StringTest.php
@@ -9,38 +9,26 @@ class StringTest extends \PHPUnit_Framework_TestCase
 
     public function testUpperCase()
     {
-        $value = (new String('teste php string'))->toUpperCase();
-        $this->assertEquals('TESTE PHP STRING', $value);
     }
 
     public function testLowerCase()
     {
-        $value = (new String('TESTE PHP STRING'))->toLowerCase();
-        $this->assertEquals('teste php string', $value);
     }
 
     public function testTrim()
     {
-        $value = (new String('   teste php string   '))->trim();
-        $this->assertEquals('teste php string', $value);
     }
 
     public function testLength()
     {
-        $length = (new String('teste php string'))->length();
-        $this->assertEquals(16, $length);
     }
 
     public function testIsEmpty()
     {
-        $string = new String();
-        $this->assertEquals(true, $string->isEmpty());
     }
 
     public function testRevese()
     {
-        $string = new String('abc');
-        $this->assertEquals('cba', $string->reverse());
     }
 
     /**
